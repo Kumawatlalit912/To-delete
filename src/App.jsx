05011ddef5a229fabbd8a591898757bbc72ab6d6
kumawatlalit007 <kumawatlalit912@gmail.com>
2023-02-11 23:11:15 +0530

    new added

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './compo/nav/Header'
+import SearchBar from './compo/search'
+
+const App = () => {
+  return (
+    <>
+    <Header />
+    <SearchBar />
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/compo/nav/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div style={{display:'flex',justifyContent:'flex-end',alignItems:'center',height:'80px'}}><ul>
+        <li>
+            Home
+        </li>
+        <li>
+            About
+        </li>
+        <li>
+            Login
+        </li>
+        </ul>
+    </div>
+  )
+}
+
+export default Header
